38117174d7820c37e7e40f35784df94ac83b8211
sachin <sachinsabbarwal@gmail.com>
2023-07-19 12:05:27 +0530

    3rd commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:hive/hive.dart';
+import 'package:sdk/ethereum_utils.dart';
+
+import 'package:sdk/utils/constants.dart';
+import 'package:web3dart/web3dart.dart';
+
+import 'account.dart';
+
+void main() async {
+  AccountsUtil accountsUtil = AccountsUtil.getInstance();
+  Hive.init("dir");
+  // var account = accountsUtil.getAccountAddress();
+  // final pKeyHex = accountsUtil.getPrivateKeyHex();
+  // printLog("private key for new account = $pKeyHex");
+  // printLog("Public address for new account = $account");
+  var etherAmount = await accountsUtil.getBalance();
+  printLog(
+      "ether balance in gwei = ${etherAmount.getValueInUnit(EtherUnit.gwei)}");
+  final wallet = await accountsUtil.getWallet();
+  sendTokens(wallet, "0x7829222A97392EFcBc743531222CB71606d6f2b4");
+  etherAmount = await accountsUtil.getBalance();
+  printLog(
+      "ether balance in gwei = ${etherAmount.getValueInUnit(EtherUnit.gwei)}");
+  Hive.close();
+}
---
+++ test/sdk_test.dart
@@ -0,0 +1,4 @@
+import 'package:sdk/main.dart';
+import 'package:test/test.dart';
+
+void main() {}
